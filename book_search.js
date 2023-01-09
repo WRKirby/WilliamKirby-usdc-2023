411aa8f4c839d7acf1ba0752079f815236a29788
WRKirby <wkirby0310@gmail.com>
2023-01-08 19:31:44 -0500

    Update book_search.js

--- book_search.js
+++ book_search.js
@@ -19,8 +19,6 @@
  * @returns {JSON} - Search results.
  * */ 
  function findSearchTermInBooks(searchTerm, scannedTextObj) {
-    /** You will need to implement your search and 
-     * return the appropriate object here. */
 
     var result = {
         "SearchTerm": "",
@@ -29,22 +27,51 @@
 
     result.SearchTerm = searchTerm;
 
-    for(var x in scannedTextObj) {
-        console.log(scannedTextObj[x]);
-        for (var y in scannedTextObj[x].Content) {
-            words = scannedTextObj[x].Content[y].Text.split(" ")
-            for(i in words) {
-                if(words[i] === searchTerm) {
+    //Split content into words.
+    words = []     //Words is a 3d array.  The rows are books, columns are the lines in the book, and depth is the content of each line.
+    for(var book in scannedTextObj) {
+        content = []
+        for (var line in scannedTextObj[book].Content) {
+            content.push(scannedTextObj[book].Content[line].Text.split(" "));
+        }
+        words.push(content);
+    }
+
+    //Combine hyphenated words
+    for(book in words){ 
+        for(line = 0; line < words[book].length; line++) {
+            lastWord = words[book][line].length - 1;
+            lastLetter = words[book][line][lastWord].length - 1;
+            if(words[book][line][lastWord][lastLetter] === '-' && line != words[book].length) {
+                words[book][line][lastWord] = words[book][line][lastWord] + words[book][line + 1][0];
+                words[book][line + 1][0] = words[book][line][lastWord];
+            }
+        }
+    }
+
+    //Remove punctuation
+    var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    for(book in words) {
+        for(line in words[book]) {
+            for(word in words[book][line]) {
+                words[book][line][word] = words[book][line][word].replace(regex, '');
+            }
+        }
+    }
+
+    //Search for words
+    for(book in words) {
+        for(line in words[book]) {
+            for(word in words[book][line]) {
+                if(words[book][line][word] === searchTerm) {
                     res = {
-                        "ISBN": scannedTextObj[x].ISBN,
-                        "Page": scannedTextObj[x].Content[y].Page,
-                        "Line": scannedTextObj[x].Content[y].Line,
+                        "ISBN": scannedTextObj[book].ISBN,
+                        "Page": scannedTextObj[book].Content[line].Page,
+                        "Line": scannedTextObj[book].Content[line].Line,
                     };
-                    result.Results.push(res);
-                    break;
+                    result.Results.push(res)
                 }
             }
-            console.log(words);
         }
     }
 
@@ -73,6 +100,27 @@ const twentyLeaguesIn = [
                 "Text": "eyes were, I asked myself how he had managed to see, and"
             } 
         ] 
+    },
+    {
+        "Title": "Ender's Shadow",
+        "ISBN": "0812575717",
+        "Content": [
+            {
+                "Page": 1,
+                "Line": 1,
+                "Text": "You think you've found somebody, so suddenly my pro-"
+            },
+            {
+                "Page": 1,
+                "Line": 2,
+                "Text": "gram gets the ax?"
+            },
+            {
+                "Page": 1,
+                "Line": 3,
+                "Text": "It's not about this kid that Graff found.  It's about the"
+            } 
+        ] 
     }
 ]
     
@@ -84,6 +132,48 @@ const twentyLeaguesOut = {
             "ISBN": "9780000528531",
             "Page": 31,
             "Line": 9
+        },
+        {
+            "ISBN": "0812575717",
+            "Page": 1,
+            "Line": 2
+        },
+        {
+            "ISBN": "0812575717",
+            "Page": 1,
+            "Line": 3
+        }
+    ]
+}
+
+const twentyLeaguesOutHyphen = {
+    "SearchTerm": "darkness",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        },
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        }
+    ]
+}
+
+const twentyLeaguesOutNegative = {
+    "SearchTerm": "guitar",
+    "Results": []
+}
+
+const twentyLeaguesOutCase = {
+    "SearchTerm": "The",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
         }
     ]
 }
@@ -114,12 +204,29 @@ if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
     console.log("Received:", test1result);
 }
 
-/** We could choose to check that we get the right number of results. */
-const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
-if (test2result.Results.length == 1) {
+test2result = findSearchTermInBooks("darkness", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOutHyphen) === JSON.stringify(test2result)) {
     console.log("PASS: Test 2");
 } else {
     console.log("FAIL: Test 2");
-    console.log("Expected:", twentyLeaguesOut.Results.length);
-    console.log("Received:", test2result.Results.length);
+    console.log("Expected:", twentyLeaguesOutHyphen);
+    console.log("Received:", test2result);
+}
+
+test3result = findSearchTermInBooks("guitar", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOutNegative) === JSON.stringify(test3result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", twentyLeaguesOutNegative);
+    console.log("Received:", test3result);
+}
+
+test4result = findSearchTermInBooks("The", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOutCase) === JSON.stringify(test4result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", twentyLeaguesOutCase);
+    console.log("Received:", test3result);
 }
